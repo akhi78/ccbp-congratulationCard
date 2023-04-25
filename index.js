7ccbf8c69d1fbbac2e553e8c5196c9ba7e7c8d13
root <akhileshrao46@gmail.com>
2023-04-25 17:02:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="BgContainer">
+    <h1 className="MainHeading">Congratulations</h1>
+    <div className="child_container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2 className="Name"> Kiran V</h2>
+      <p className="description">
+        Vishnu Institute of computer education and technology Bhopal
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
